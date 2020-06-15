479a718f2cfc39e68cbe3a5c8810429f8b14dbd6
Walter <walterxie@users.noreply.github.com>
2020-06-15 14:47:36 +1200

    fix write bug

--- R/Alignment.R
+++ R/Alignment.R
@@ -44,14 +44,14 @@ countCharFreq <- function(seqs.df, attr.taxon="taxon", attr.seq="value", CASE.FU
 #' @param file the fasta file name.
 #' @export
 #' @examples
-#' writeFasta(seqs.df)
+#' writeFasta(seqs.df, file="seqs.fasta")
 #'
 #' @rdname Alignment
 writeFasta <- function(seqs.df, file="seqs.fasta", attr.taxon="taxon", attr.seq="value", CASE.FUN=toupper) {
   cat("",file=file)
   for(i in 1:nrow(seqs.df)) {
-    cat(">", seqs.df[i,attr.taxon], "\n", file=file, append=TRUE)
-    cat(">", seqs.df[i,attr.seq], "\n", file=file, append=TRUE)
+    cat(paste0("> ", seqs.df[i,attr.taxon], "\n"), file=file, append=TRUE)
+    cat(paste0(seqs.df[i,attr.seq], "\n"), file=file, append=TRUE)
   }
 }
 
