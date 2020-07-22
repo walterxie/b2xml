1db0263895fc9e3b64dd97a990db8d46079984fd
Walter <walterxie@users.noreply.github.com>
2020-07-22 17:14:03 +1200

    fix plot

--- R/MetadataPlot.R
+++ R/MetadataPlot.R
@@ -12,6 +12,7 @@
 #' @export
 #' @examples
 #' meta.df <- inner_join(dates.df, loc.df, by = "taxon")
+#' stopifnot(nrow(meta.df)==nrow(dates.df) && nrow(meta.df)==nrow(loc.df))
 #' plotLocDate(meta.df)
 #'
 #' @rdname Plot
@@ -19,15 +20,16 @@ plotLocDate <- function(meta.df, date.col="date", trait.col="trait",
                         x.lab="Date", y.lab="Number of samples", legend="locations") {
   require(scales)
   require(ggplot2)
-  if (!all(is.na(as.Date(as.character(meta.df[date.col]),format="%d/%m/%Y"))))
+  if (!all(is.na(as.Date(as.character(meta.df[[date.col]]),format="%d/%m/%Y"))))
     stop("Column ", date.col, " requires date, such as yyyy-mm-dd !")
   # count by region coloured by month
-  ggplot(meta.df, aes_string(x = date.col, fill = trait.col,
+  p <- ggplot(meta.df, aes_string(x = date.col, fill = trait.col,
                                   group = paste0("interaction(",date.col,",",trait.col,")"))) +
-    geom_bar() + scale_x_date(labels = date_format("%m-%Y")) +
-    ggtitle(paste(nrow(meta),"samples from", length(unique(meta[trait.col])), legend,
-                  "between", date_format("%d-%m-%Y")(min(meta.df[date.col])),
-                  "and", date_format("%d-%m-%Y")(max(meta.df[date.col])))) +
+    geom_bar() + scale_x_date(labels = date_format("%m-%Y"))
+
+  p <- p + ggtitle(paste(nrow(meta.df),"samples from", length(unique(meta.df[[trait.col]])), legend,
+                  "between", date_format("%d-%m-%Y")(min(meta.df[[date.col]])),
+                  "and", date_format("%d-%m-%Y")(max(meta.df[[date.col]])))) +
     xlab(x.lab) + ylab(y.lab) + labs(fill=legend) +
     theme_minimal()
 }
